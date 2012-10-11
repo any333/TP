b856ec70f09130680d4110fb1883702dad1b2d24
Anna-Mariya <any333@abv.bg>
2012-10-11 14:01:48 +0300

    Version.java

--- h5/Version.java
+++ h5/Version.java
@@ -9,25 +9,32 @@
 показани в изхода трябва да са сортиран във възходящ ред. Ако последната стойност е
 down изходът трябва да е сортиран в низходящ ред. Стойността по подразбиране, ако не е
 посочено up или down, е up.
+ Пример:
+java HomeWork1 java.version java.vm.version java.vendor invalid java.vendor.url down
+трябва до доведе до
+http://java.sun.com/
+Sun Microsystems Inc.
+11.0-b15
+1.6.0_10
 */
 
 public class Version{  
-	public static void main(String [] argv){
-	int t;
-	int b=0;
-	String m;
-	for(t=0;t<=argv.length-1;t++)
-		if(argv[t].equals("down"))
-			b=1;
-	if (b==1){
+	public static void main (String [] argv){
+	int i;
+	int count_down = 0;
+	String s;
+	for(i = 0; i <= argv.length-1; i++)
+		if(argv[i].equals("down"))
+			count_down = 1;
+	if (count_down == 1){
 		java.util.Arrays.sort(argv,java.util.Collections.reverseOrder());
 	} else{
 		java.util.Arrays.sort(argv);
 	}
-	for(t=0;t<=argv.length-1;t++){
-		m=System.getProperty(argv[t]);
-		if(m!=null)
-			System.out.println(m); 
+	for(i = 0; i <= argv.length-1; i++){
+		s = System.getProperty(argv[i]);
+		if(s != null)
+			System.out.println(s); 
 	}
 	}
 }
